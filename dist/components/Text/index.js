ae78ff36399b31dd2af8e8b2ea518a16fb1d45f3
david <dbzx-10299@hotmail.com>
2025-01-10 21:42:02 +0100

    Remove the now ignored directory: dist

--- dist/components/Text/index.js
+++
@@ -1,4 +0,0 @@
-import { _ as f } from "../../index.vue_vue_type_script_setup_true_lang-CTB7hvfH.js";
-export {
-  f as default
-};
--- dist/main.js
+++
@@ -1,4 +0,0 @@
-import { _ as r } from "./index.vue_vue_type_script_setup_true_lang-CTB7hvfH.js";
-export {
-  r as Text
-};
